4ab83a7bb1f0ca9269028e493dac74a6eb2ae1a8
Afif Fatchur Rahman <49427338+apipp@users.noreply.github.com>
2023-06-15 11:39:47 +0700

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -21,15 +21,6 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KYDMBFFF85"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-KYDMBFFF85');
-        </script>
       </Head>
       <Body>
         <Suspense>
