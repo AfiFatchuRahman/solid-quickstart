ea761e1e872856e3ee583f6d0b9343373166e933
Afif Fatchur Rahman <afif@koinworks.com>
2023-06-15 16:41:15 +0700

    fix :: adjust measurement ID

--- src/root.tsx
+++ src/root.tsx
@@ -21,7 +21,7 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KYDMBFFF85"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DNCKGWZY07"></script>
         <script type="text/javascript" src="gtm.js"></script>
       </Head>
       <Body>
